399954720d648c3a4bf3748f8ef3ede9ed711897
prasantib <prasanti.babu@gmail.com>
2019-10-23 13:40:51 -0400

    fizzbuzzjava

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int n = 100;
+        for (int i=1; i<=n; i++)
+        {
+            if (i%15==0)
+                System.out.println("FizzBuzz"+" ");
+            else if (i%5==0)
+                System.out.println("Buzz"+" ");
+            else if (i%3==0)
+                System.out.println("Fizz"+" ");
+            else
+                System.out.print(i+" ");
+        }
+    }
+}
